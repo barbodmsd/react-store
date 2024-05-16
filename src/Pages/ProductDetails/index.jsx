59a602de2a87273bfe84a12f223fd03141bc16d6
Barbodmsd <barbodmasoudi@gmail.com>
2024-05-17 01:12:59 +0430

    end useeffect

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function ProductDetails() {
-    const [product,setProduct]=useState()
-  return (
-    <div>ProductDetails</div>
-  )
+  const [product, setProduct] = useState();
+  const { id } = useParams(); // get the id slug
+  const navigate=useNavigate()
+  // get one product
+  useEffect(()=>{
+    (async()=>{
+        try {
+            const res=await fetch(`https://fakestoreapi.com/products/${id}`)
+            const data=await res.json()
+            data?setProduct(data):navigate('/product-not-found')
+        } catch (error) {
+         toast.error('Network error!')   
+        }
+    })()
+  },[id])
+  return <></>;
 }
