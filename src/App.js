2d9da3263c1d8cecb379697a51b0947d568e8053
Barbodmsd <barbodmasoudi@gmail.com>
2024-05-16 23:52:38 +0430

    write routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import { Box } from "@mui/material";
-import { Routes,Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import ProductDetails from "./Pages/ProductDetails";
+import Auth from "./Pages/Auth";
+import Cart from "./Pages/Cart";
+import NotFound from "./Pages/NotFound";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Box id={'master'}>
+      {/* write routes */}
+      <Box id={"master"}>
         <Routes>
-          <Route exact path={'/'} element={<Home}/>
+          <Route exact path={"/"} element={<Home />} />
+          <Route exact path={"/products"} element={<Products />} />
+          <Route
+            exact
+            path={"/product-details/:id/:name"}
+            element={<ProductDetails />}
+          />
+          <Route exact path={"/auth"} element={<Auth />} />
+          <Route exact path={"/cart"} element={<Cart />} />
+          <Route exact path={"*"} element={<NotFound />} />
         </Routes>
       </Box>
       <Footer />
