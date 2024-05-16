cda30ccb3adbe7353e733d50fb2f98f7844fefae
Barbodmsd <barbodmasoudi@gmail.com>
2024-05-16 23:15:16 +0430

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* provide store to react*/}
+      <BrowserRouter>
+        {/* for use from react router dom */}
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
