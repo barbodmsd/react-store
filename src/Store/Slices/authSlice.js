7c87fe5b71253fbc2a0fd7b84ab200f441bf6c60
Barbodmsd <barbodmasoudi@gmail.com>
2024-05-16 23:10:57 +0430

    end cart slice

--- src/Store/Slices/authSlice.js
+++ src/Store/Slices/authSlice.js
@@ -7,6 +7,7 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  // actions
   reducers: {
     login: (state, action) => {
       state.token = action.payload;
--- src/Store/Slices/cartSlice.js
+++ src/Store/Slices/cartSlice.js
@@ -1,3 +1,48 @@
-const initialState={
-    list:[]
-}
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  list: [],
+};
+
+// create cart slice
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  // actions
+  reducers: {
+    clear: (state) => {
+      state.list = [];
+    },
+    removeItem: (state, action) => {
+      state.list = state.list.filter((e) => {
+        // if the item was in cart
+        if (e.id === action.payload) {
+          e.quantity = e.quantity - 1;
+          if (e.quantity === 0) {
+            return false; // delete item when the count be zero
+          }
+          return e; //return the new item (items was changed his count)
+        }
+        return true; // put item in his place
+      });
+    },
+    addItem: (state, action) => {
+      let add = false;
+      state.list = state.list.map((e) => {
+        // if items was in the cart
+        if (e.id === action.payload.id) {
+          e.quantity = e.quantity + 1;
+          return e; //return the new item (items was changed his count)
+        }
+        return e; // put item in his place
+      });
+      // if item not in cart then add it in the cart list
+      if (!add) {
+        state.list.push({ ...action.payload, quantity: 1 });
+      }
+    },
+  },
+});
+export const { clear, removeItem, addItem } = cartSlice.actions;
+export default cartSlice.reducer;
+// export the all we need
