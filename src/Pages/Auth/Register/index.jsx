664f9688504e3a51bcca9dc3d101e6b6fcf3e375
Barbodmsd <barbodmasoudi@gmail.com>
2024-05-17 17:33:43 +0430

    start auth branch

--- src/Pages/Auth/Register/index.jsx
+++ src/Pages/Auth/Register/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
+    const [pageType,setPageType]=useState('login')
   return (
     <div>Register</div>
   )
